788fb9355b3ab6e969715b93761afd1b6a013bf8
IraSit11 <iriasitnova@yandex.ru>
2025-09-14 01:38:48 +0300

    Delete src/test/java/BaseTest.java

--- src/test/java/BaseTest.java
+++
@@ -1,9 +0,0 @@
-import io.restassured.RestAssured;
-import org.junit.BeforeClass;
-
-public class BaseTest {
-    @BeforeClass
-    public static void setup() {
-        RestAssured.baseURI = "https://stellarburgers.nomoreparties.site/";
-    }
-}
